77f28d1218281fe5107c7551e023753b44b69fb2
abhijitmorye <abhijitmorye>
2022-12-10 04:06:37 -0500

    install.shi-python3 added

--- install_run.sh
+++ install_run.sh
@@ -5,4 +5,4 @@ set -x
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 pip3 install -r ${SCRIPT_DIR}/requirements.txt && \
-python ${SCRIPT_DIR}/app.py $1
+python3 ${SCRIPT_DIR}/app.py $1
