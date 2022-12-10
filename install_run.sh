bf7075c65670a2b011565c5650cd8af3a37a46ca
abhijitmorye <abhijitmorye>
2022-12-10 04:01:26 -0500

    install.sh modified

--- install_run.sh
+++ install_run.sh
@@ -4,5 +4,5 @@ set -x
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-pip install -r ${SCRIPT_DIR}/requirements.txt && \
+pip3 install -r ${SCRIPT_DIR}/requirements.txt && \
 python ${SCRIPT_DIR}/app.py $1
